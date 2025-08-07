0912d25878f696fe907bd9f7156763ebacc24bd6
alammrshah <theshahalam63@gmail.com>
2025-08-07 11:49:42 +0530

    Revert "erge branch 'master' of https://github.com/alammrshah/Microservices-By-Bits-And-Bytes"
    This reverts commit 3d4a2e48f2a4db2f06e639c67cb2edf92827a29e, reversing
    changes made to 4a5cf30219fd9df775e5edc389a9ef79c3bfc867.

--- Microservices/product-service/src/main/java/com/ecommerce/product_service/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ecommerce.product_service.repository;
-
-import com.ecommerce.product_service.entity.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, String> {
-}
